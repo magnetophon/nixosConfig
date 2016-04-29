a1f0f7cd311b86ac3f410798b67c7fdbc777e9d6
Bart Brouns <bart@magnetophon.nl>
2016-04-29 14:42:49 +0200

    import music in default mixos

--- machines/mixos/default.nix
+++ machines/mixos/default.nix
@@ -7,9 +7,26 @@
       # on every machine:
       ../../common.nix
       # music tweaks and progs:
-      # ../../music.nix
+       ../../music.nix
     ];
 
   networking.hostName = "mixos";
-}
 
+#for running alsa trough jack
+boot.kernelModules = [ "snd-aloop" ];
+#sound.enableMediaKeys = true;
+
+  musnix = {
+    enable = true;
+    kernel.optimize = true;
+    kernel.realtime = true;
+    # kernel.packages = pkgs.linuxPackages_4_1_rt;
+    /*kernel.latencytop = true;*/
+    soundcardPciId = "04:01";
+    rtirq.nameList = "rtc0 snd_rme9652";
+    rtirq.nonThreaded = "rtc0 snd_rme9652";
+    /*rtirq.nameList = "rtc0 usb";*/
+    /*rtirq.nameList = "rtc0 hpet usb";*/
+    /*rtirq.nameList = "rtc0 hpet snd snd_hda_intel";*/
+  };
+}
