b4a162378c935086e265d91bb108b830a4fc146f
Bart Brouns <bart@magnetophon.nl>
2016-08-01 19:11:41 +0200

    compnix: WIP resolution

--- machines/compnix/machine.nix
+++ machines/compnix/machine.nix
@@ -69,17 +69,17 @@ in
   #'';
 
 
-services.xserver.monitorSection = ''
-  Identifier "VGA1"
-  Modeline "1920x1080_60.00" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
-  Option "PreferredMode" "1920x1080_60.00"
-'';
-services.xserver.screenSection = ''
-  Identifier "Screen0"
-  Monitor "VGA1"
-  DefaultDepth 24
-  SubSection "Display"
-    Modes "1920x1080_60.00"
-  EndSubSection
-'';
+# services.xserver.monitorSection = ''
+#   Identifier "VGA1"
+#   Modeline "1920x1080_60.00" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
+#   Option "PreferredMode" "1920x1080_60.00"
+# '';
+# services.xserver.screenSection = ''
+#   Identifier "Screen0"
+#   Monitor "VGA1"
+#   DefaultDepth 24
+#   SubSection "Display"
+#     Modes "1920x1080_60.00"
+#   EndSubSection
+# '';
 }
