ec27f18be53f3914c1eec2a44a281ac1e85afc09
Bart Brouns <bart@magnetophon.nl>
2017-03-05 21:00:04 +0100

    compnix: add music conf

--- machines/compnix/default.nix
+++ machines/compnix/default.nix
@@ -7,31 +7,28 @@
       # on every machine:
       ../../common.nix
       # music tweaks and progs:
-      # ../../music.nix
+      ../../music.nix
       #/home/bart/source/musnix/default.nix
     ];
 
   networking.hostName = "compnix";
 
-# musnix = {
-#   enable = true;
-#   kernel.packages = pkgs.linuxPackages_latest_rt;
-#   #kernel.latencytop = true;
-#   rtirq.enable = true;
-#   # machine specific:
-#   /*soundcardPciId = "00:1b.0";*/
-#   /*rtirq.nameList = "rtc0 hpet snd snd_hda_intel";*/
-#   # rtirq.highList = "snd_hrtimer";
-#   /*rtirq.nonThreaded = "rtc0 snd";*/
-#   rtirq.resetAll = 1;
-#
-#   alsaSeq.enable = false;
-#   kernel.optimize = true;
-#   kernel.realtime = true;
-#   # soundcardPciId = "04:01";
-#
-#   # rtirq.nameList = "rtc0 snd_rme9652";
-#   # rtirq.nonThreaded = "rtc0 snd_rme9652";
-# };
-#
+  musnix = {
+    alsaSeq.enable = false;
+    kernel.optimize = true;
+    kernel.realtime = true;
+    # kernel.packages = pkgs.linuxPackages_4_1_rt;
+    kernel.packages = pkgs.linuxPackages_latest_rt;
+    /*kernel.latencytop = true;*/
+    # soundcardPciId = "04:01";
+
+    # rtirq.nameList = "rtc0 snd_rme9652";
+    rtirq.nameList = "rtc0 usb";
+    # rtirq.nameList = "rtc0 snd";
+
+    # rtirq.nonThreaded = "rtc0 snd_rme9652";
+    /*rtirq.nameList = "rtc0 usb";*/
+    /*rtirq.nameList = "rtc0 hpet usb";*/
+    /*rtirq.nameList = "rtc0 hpet snd snd_hda_intel";*/
+  };
 }
