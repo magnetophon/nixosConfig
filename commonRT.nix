8eec1c02280b73dc1471b263cb0871bafb3a2c86
Bart Brouns <bart@magnetophon.nl>
2020-10-12 21:03:34 +0200

    use nixos rt kernel instead of musnix

--- commonRT.nix
+++ commonRT.nix
@@ -6,14 +6,15 @@
 
   # boot.kernelPackages = pkgs.linux_rt.linux_4_19_rt;
   # boot.kernelPackages = pkgs.linux_rt.linux_5_0_rt;
+  boot.kernelPackages = pkgs.linuxPackages-rt_latest;
 
   musnix = {
     enable = true;
-    kernel.packages = pkgs.linuxPackages_latest_rt;
+    # kernel.packages = pkgs.linuxPackages_latest_rt;
     # kernel.packages = pkgs.linuxPackages_5_0_rt;
     # kernel.packages = pkgs.linuxPackages_4_19_rt;
-    kernel.optimize = true;
-    kernel.realtime = true;
+    # kernel.optimize = true;
+    # kernel.realtime = true;
     rtirq.enable = true;
     das_watchdog.enable = true;
   };
