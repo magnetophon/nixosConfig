580985796d40989856cce71b2c9be97a4fbd63e1
Bart Brouns <bart@magnetophon.nl>
2021-04-12 18:05:41 +0200

    tlp: config for nix14

--- machines/nix14/default.nix
+++ machines/nix14/default.nix
@@ -13,8 +13,46 @@ with pkgs; {
 
   networking = { hostName = "nix14"; };
 
-  services.tlp.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0 = "70";
+      STOP_CHARGE_THRESH_BAT0 = "90";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_BOOST_ON_AC = "1";
+      CPU_BOOST_ON_BAT = "0";
+      CPU_HWP_DYN_BOOST_ON_AC = "1";
+      CPU_HWP_DYN_BOOST_ON_BAT = "0";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CIE_ASPM_ON_AC = "performance";
+      CIE_ASPM_ON_BAT = "powersupersave";
+      INTEL_GPU_MIN_FREQ_ON_AC = "300";
+      INTEL_GPU_MIN_FREQ_ON_BAT = "300";
+      INTEL_GPU_MAX_FREQ_ON_AC = "1150";
+      INTEL_GPU_MAX_FREQ_ON_BAT = "300";
+      INTEL_GPU_BOOST_FREQ_ON_AC = "1150";
+      # INTEL_GPU_BOOST_FREQ_ON_BAT = "1150";
+      ENERGY_PERF_POLICY_ON_BAT = "powersave";
+      # DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "wifi";
+      DEVICES_TO_DISABLE_ON_LAN_CONNECT = "wifi";
+      # DEVICES_TO_ENABLE_ON_LAN_DISCONNECT = "wifi";
+      # Enable audio power saving for Intel HDA, AC97 devices (timeout in secs).
+      # A value of 0 disables, >= 1 enables power saving.
+      # Note: 1 is recommended for Linux desktop environments with PulseAudio,
+      # systems without PulseAudio may require 10.
+      # Default: 1
 
+      SOUND_POWER_SAVE_ON_AC = "0";
+      SOUND_POWER_SAVE_ON_BAT = "1";
+      # Disable controller too (HDA only): Y/N.
+      # Note: effective only when SOUND_POWER_SAVE_ON_AC/BAT is activated.
+      # Default: Y
+      # SOUND_POWER_SAVE_CONTROLLER = "N";
+      USB_EXCLUDE_PHONE = "1";
+    };
+  };
   # hardware.pulseaudio.enable = true;
 
   # boot.kernelPackages = pkgs.linuxPackages_4_19;
