7ee78c8579a657f6c7296826f8a939ab3973bd32
Bart Brouns <bart@magnetophon.nl>
2025-10-03 11:41:52 +0200

    nixframe: fix overheating

--- machines/nixframe/default.nix
+++ machines/nixframe/default.nix
@@ -21,24 +21,22 @@ with pkgs; {
       settings = {
         START_CHARGE_THRESH_BAT0 = "70";
         STOP_CHARGE_THRESH_BAT0 = "90";
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        # CPU_BOOST_ON_AC = "1";
+        CPU_BOOST_ON_AC = "1";
         CPU_BOOST_ON_BAT = "0";
-        # CPU_HWP_DYN_BOOST_ON_AC = "1";
+        CPU_HWP_DYN_BOOST_ON_AC = "1";
         CPU_HWP_DYN_BOOST_ON_BAT = "0";
-        # CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        # CIE_ASPM_ON_AC = "performance";
-        CIE_ASPM_ON_BAT = "powersupersave";
+        PCIE_ASPM_ON_AC = "default";
+        PCIE_ASPM_ON_BAT = "powersupersave";
         # INTEL_GPU_MIN_FREQ_ON_AC = "300";
         INTEL_GPU_MIN_FREQ_ON_BAT = "100";
         # INTEL_GPU_MAX_FREQ_ON_AC = "1300";
         INTEL_GPU_MAX_FREQ_ON_BAT = "300";
         # INTEL_GPU_BOOST_FREQ_ON_AC = "1300";
         INTEL_GPU_BOOST_FREQ_ON_BAT = "300";
-
-        PCIE_ASPM_ON_BAT = "powersupersave";
 
         ENERGY_PERF_POLICY_ON_BAT = "powersave";
         # DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "wifi";
